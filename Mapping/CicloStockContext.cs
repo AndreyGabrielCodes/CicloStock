ff67d082bee9ab2bf6d4222df2fd4f64f1f6b5b7
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-03-24 06:57:07 -0300

    Iniciado criação do Mapping

---
+++ Mapping/CicloStockContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CicloStock.Models;
+
+namespace CicloStock.Mapping
+{
+    public class CicloStockContext : DbContext
+    {
+        public DbSet<EntradaModel> Entrada { get; set; }
+        public DbSet<EntradaLoteModel> EntradaLote { get; set; }
+        public DbSet<SaidaModel> Saida { get; set; }
+        public DbSet<SaidaLoteModel> SaidaLote { get; set; }
+        public DbSet<LocacaoModel> Locacao { get; set; }
+        public DbSet<ProdutoModel> Produto { get; set; }
+        public DbSet<ProdutoLocacaoModel> ProdutoLocacao { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=CicloStock;User ID=sa;Password=1q2w3e4r@#$");
+        }
+
+    }
+}
---
+++ Mapping/EntradaMap.cs
@@ -0,0 +1,20 @@
+﻿using CicloStock.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CicloStock.Mapping
+{
+    public class EntradaMap : IEntityTypeConfiguration<EntradaModel>
+    {
+        public void Configure(EntityTypeBuilder<EntradaModel> builder)
+        {
+            builder.ToTable("Entrada");
+
+            builder.HasKey(x => x.EntradaId);
+            builder.Property(x => x.EntradaId)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+        }
+    }
+}
