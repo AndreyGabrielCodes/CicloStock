b053c595bd837fb19811ce1694e859336dfdc9d0
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-03-28 20:52:10 -0300

    Retirado enumerados do modulo de saida

--- Utilitarios/Enumerados.cs
+++ Utilitarios/Enumerados.cs
@@ -16,20 +16,6 @@
             Concluido = 2,
             Cancelado = 3,
         } 
-        public enum SituacaoSaida : short 
-        {
-            Aberto = 0,
-            EmAndamento = 1,
-            Concluido = 2,
-            Cancelado = 3,
-        }
-        public enum SituacaoSaidaLote : short 
-        {
-            Aberto = 0,
-            EmAndamento = 1,
-            Concluido = 2,
-            Cancelado = 3,
-        }
         public enum SituacaoProduto : short
         {
             Inativo = 0,
