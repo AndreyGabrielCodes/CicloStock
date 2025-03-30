3f7b79a7ade40039b93dc964ac25cbe2eaf619dd
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-03-30 15:30:44 -0300

    Alterado alguns arquivos do projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CicloStock.Mapping;
+using CicloStock.Models;
+using CicloStock.Operacoes;
+using CicloStock.Utilitarios;
+using System;
+
+
+namespace CicloStock.Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
